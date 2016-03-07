fc65ed1d92096b4d41dd7d6004804adf504aa1bc
mikejeffers <mike.s.jeffers@gmail.com>
2016-03-06 20:20:41 -0500

    views for simple home request

--- framework/views.py
+++ framework/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = {}
+    context['errors'] = []
+    context['messages'] = []
+    # in template check for user.is_authenticated
+    context['user'] = request.user
+    return render(request, 'main.html', context)

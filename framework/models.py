896d138f9d1447e5e2aacc2035d90b24055bebff
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-07 22:31:40 -0500

    Added a CalendarUser model which wraps the base Django User.
    This closes #11.

--- framework/models.py
+++ framework/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class CalendarUser(models.Model):
+    user = models.OneToOneField(User)

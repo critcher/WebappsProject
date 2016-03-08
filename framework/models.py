bd0da1d7db3c72c19924fb3fa1a8b8c967621672
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-07 23:10:53 -0500

    Added a model representing an App.
    Closes #5.

--- framework/models.py
+++ framework/models.py
@@ -16,3 +16,21 @@ class Event(models.Model):
     end_timestamp = models.DateTimeField()
     icon_url = models.CharField(blank=True, max_length=256)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+class App(models.Model):
+    description = models.CharField(blank=True, max_length=1000)
+    name = models.CharField(max_length=60)
+    # Can be used to detect that a user's AppSettings are out of
+    # date and might need to be prompted again
+    version = models.CharField(max_length=60)
+    icon_url = models.CharField(blank=True, max_length=256)
+    # URL that we send a GET request to in order to find out what
+    # settings the user can set. If blank, no special settings
+    # are needed
+    settings_url = models.CharField(blank=True, max_length=256)
+    # URL that we send the request to in order to get the actual
+    # data from the app
+    data_url = models.CharField(max_length=256)
+    # True if the app should be allowed to be added to a calendar
+    # multiple times (with different settings, maybe)
+    allow_duplicates = models.BooleanField(default=True)

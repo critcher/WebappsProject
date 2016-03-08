20abe5bfc3d6b7fb488a200f4f8062973bf6251d
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-07 22:55:31 -0500

    Added an Event model.
    This closes #9.

--- framework/models.py
+++ framework/models.py
@@ -2,10 +2,16 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.core.validators import MaxValueValidator
 
 class CalendarUser(models.Model):
     user = models.OneToOneField(User)
 
 class Calendar(models.Model):
     owner = models.OneToOneField(CalendarUser, on_delete=models.CASCADE)
+
+class Event(models.Model):
+    description = models.CharField(blank=True, max_length=100)
+    start_timestamp = models.DateTimeField()
+    end_timestamp = models.DateTimeField()
+    icon_url = models.CharField(blank=True, max_length=256)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)

4cf132a2175a65af9d0e802c270b2b364b92556a
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-07 23:24:09 -0500

    Added the AppSettings model.
    Closes #6.

--- framework/models.py
+++ framework/models.py
@@ -16,6 +16,9 @@ class Event(models.Model):
     end_timestamp = models.DateTimeField()
     icon_url = models.CharField(blank=True, max_length=256)
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+    # A handle to the AppSettings instance that created this event.
+    # Blank if this event is a static calendar event.
+    source = models.ForeignKey(AppSettings, blank=True, on_delete=models.CASCADE)
 
 class App(models.Model):
     description = models.CharField(blank=True, max_length=1000)
@@ -34,3 +37,15 @@ class App(models.Model):
     # True if the app should be allowed to be added to a calendar
     # multiple times (with different settings, maybe)
     allow_duplicates = models.BooleanField(default=True)
+
+class AppSettings(models.Model):
+    settings_json = models.TextField()
+    # Can be used to detect that a user's AppSettings are out of
+    # date and might need to be prompted again
+    version = models.CharField(max_length=60)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    # Used to let the app know when we last asked for information
+    # for this user. Note that the app can still give us data
+    # for dates before this timestamp (to update changed events,
+    # for example)
+    last_updated_timestamp = models.DateTimeField(blank=True)

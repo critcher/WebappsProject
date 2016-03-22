a504c3cd0247a60c25386b77e04a9be895a623f6
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-21 21:56:29 -0400

    Adding json validation code.

---
+++ framework/appForms.py
@@ -0,0 +1,18 @@
+from jsonschema import validate, ValidationError
+import json
+
+""" Throws IOError if the file is invalid, and ValueError
+    if the json is invalid.
+"""
+def getSchemaFromFile(filename):
+    with open(filename, 'r') as f:
+        schemaText = f.read()
+        schemaDict = json.loads(schemaText)
+    return schemaDict
+
+""" Throws ValueError if the json is invalid, and
+    ValidationError if the json does not fit the schema.
+"""
+def validateFromString(schema, jsonString):
+    jsonDict = json.loads(jsonString)
+    validate(jsonDict, schema)

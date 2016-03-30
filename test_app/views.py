cc631d4446103b293d6fa13cdb3bab7fd220a7ee
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-30 17:23:19 -0400

    Added a skeleton project and app with url routing to act as an app.

---
+++ test_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponseBadRequest, JsonResponse
+import json
+
+def getEvents(request):
+    return JsonResponse({})
+
+def formHandling(request):
+    return JsonResponse({})

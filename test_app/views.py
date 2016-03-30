8e2d8e6af31afed4b477384b10a5ad097112a6ff
Clayton Ritcher <claytonritcher@gmail.com>
2016-03-30 18:03:39 -0400

    Updated the event view to return a single event at the current time.

--- test_app/views.py
+++ test_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
+import datetime
 
+@csrf_exempt
 def getEvents(request):
-    return JsonResponse({})
+    events = []
+    s = datetime.datetime.now()
+    e = s.replace(hour = s.hour + 1)
+    format = '%Y-%m-%dT%H:%MZ'
+    events.append({'title': 'test', 'start': s.strftime(format), 'e': e.strftime(format)})
+    return JsonResponse(events, safe=False)
 
+@csrf_exempt
 def formHandling(request):
     return JsonResponse({})

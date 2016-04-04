81b43b7d1b87774396ee92b4f6efa27594777b6e
Clayton Ritcher <claytonritcher@gmail.com>
2016-04-04 16:59:45 -0400

    Cleaned up the form population javascript a little.

--- framework/static/framework/js/appForm.js
+++ framework/static/framework/js/appForm.js
@@ -1,3 +1,21 @@
+// Get the json for all of the forms
+formUrls.map(function(setting) {
+    var jqxhr = $.getJSON( setting['url']);
+
+    jqxhr.done(function(data) {
+        var jqxhr2 = $.post( json2form, {"id": setting['id'], "data":JSON.stringify(data), 'csrfmiddlewaretoken': csrf});
+        jqxhr2.done(function(data){
+            var domElement = $("#App" + setting['id']);
+            domElement.html(data);
+            domElement.find("script").each(function(i) {
+                    eval($(this).text());
+            });
+        });
+        
+    });
+});
+ 
+
 function submitted(event) {
     event.preventDefault();
     var form = $(event.target);

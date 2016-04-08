b5f23ff5f617aa74454c6c35092e437dc326df8a
Clayton Ritcher <claytonritcher@gmail.com>
2016-04-08 19:59:35 -0400

    Revamped the sports app to get all games for the team and some data in the description.

--- test_app/views.py
+++ test_app/views.py
@@ -4,39 +4,50 @@ import json
 import datetime
 import urllib2
 
-teamMapping = {"Arizona Diamondbacks": "arizona-diamondbacks",
-               "Atlanta Braves": "atlanta-braves",
-               "Baltimore Orioles": "baltimore-orioles",
-               "Boston Red Sox": "boston-red-sox",
-               "Chicago Cubs": "chicago-cubs",
-               "Chicago White Sox": "chicago-white-sox",
-               "Cincinnati Reds": "cincinnati-reds",
-               "Cleveland Indians": "cleveland-indians",
-               "Colorado Rockies": "colorado-rockies",
-               "Detroit Tigers": "detroit-tigers",
-               "Houston Astros": "houston-astros",
-               "Kansas City Royals": "kansas-city-royals",
-               "Los Angeles Angels": "los-angeles-angels",
-               "Los Angeles Dodgers": "los-angeles-dodgers",
-               "Miami Marlins": "miami-marlins",
-               "Milwaukee Brewers": "milwaukee-brewers",
-               "Minnesota Twins": "minnesota-twins",
-               "New York Mets": "new-york-mets",
-               "New York Yankees": "new-york-yankees",
-               "Oakland Athletics": "oakland-athletics",
-               "Philadelphia Phillies": "philadelphia-phillies",
-               "Pittsburgh Pirates": "pittsburgh-pirates",
-               "San Diego Padres": "san-diego-padres",
-               "San Francisco Giants": "san-francisco-giants",
-               "Seattle Mariners": "seattle-mariners",
-               "St. Louis Cardinals": "st-louis-cardinals",
-               "Tampa Bay Rays": "tampa-bay-rays",
-               "Texas Rangers": "texas-rangers",
-               "Toronto Blue Jays": "toronto-blue-jays",
-               "Washington Nationals": "washington-nationals"
+teamMapping = {"Arizona Diamondbacks": 109,
+               "Atlanta Braves": 144,
+               "Baltimore Orioles": 110,
+               "Boston Red Sox": 111,
+               "Chicago Cubs": 112,
+               "Chicago White Sox": 145,
+               "Cincinnati Reds": 113,
+               "Cleveland Indians": 114,
+               "Colorado Rockies": 115,
+               "Detroit Tigers": 116,
+               "Houston Astros": 117,
+               "Kansas City Royals": 118,
+               "Los Angeles Angels": 108,
+               "Los Angeles Dodgers": 119,
+               "Miami Marlins": 146,
+               "Milwaukee Brewers": 158,
+               "Minnesota Twins": 142,
+               "New York Mets": 121,
+               "New York Yankees": 147,
+               "Oakland Athletics": 133,
+               "Philadelphia Phillies": 143,
+               "Pittsburgh Pirates": 134,
+               "San Diego Padres": 135,
+               "San Francisco Giants": 137,
+               "Seattle Mariners": 136,
+               "St. Louis Cardinals": 138,
+               "Tampa Bay Rays": 139,
+               "Texas Rangers": 140,
+               "Toronto Blue Jays": 141,
+               "Washington Nationals": 120
                }
-query = "https://api.seatgeek.com/2/events?datetime_utc.gte=%s&datetime_utc.lte=%sT21:59:59&performers.slug=%s&page=%d"
+query = "http://mlb.com/lookup/json/named.schedule_team_sponsors.bam?start_date='%s'&end_date='%s'&team_id=%d&season=%d&game_type='R'&game_type='A'"
 output_format = '%Y-%m-%dT%H:%MZ'
+input_format = '%Y-%m-%d'
+mlb_format = '%Y/%m/%d'
+
+teamRecordStr = "%s (%s)"
+pitcherStr = "%s (%s, %s)"
+
+def buildTeamDesc(team, wl, prob, probWl, probEra):
+    description = teamRecordStr % (team, wl)
+    if prob != "":
+        description += ": " + pitcherStr % (prob, probWl, probEra)
+    return description
 
 @csrf_exempt
 def getEvents(request):
@@ -45,23 +56,36 @@ def getEvents(request):
         tmp = json.loads(request.GET['settings'])
         team = tmp['Team']['value']
     except Exception, e:
-        print e
+        pass
     events = []
 
     moreEvents = True
     pageNum = 1
     try:
-        while moreEvents:
-            response = urllib2.urlopen(query % (request.GET['start'], request.GET['end'], teamMapping[team], pageNum))
-            pageNum += 1
-            data = json.load(response)
-            moreEvents = (pageNum * data["meta"]["per_page"] < data["meta"]["total"])
-            curEvents = data["events"]
-            for ev in curEvents:
-                s = datetime.datetime.strptime(ev["datetime_utc"], "%Y-%m-%dT%H:%M:%S")
-                e = s + datetime.timedelta(hours = 3)
-                events.append({'title': ev["title"], 'start': s.strftime(output_format), 'end': e.strftime(output_format)})
-    except KeyError:
+        start = datetime.datetime.strptime(request.GET['start'], input_format)
+        end = datetime.datetime.strptime(request.GET['end'], input_format)
+        response = urllib2.urlopen(query % (start.strftime(mlb_format), end.strftime(mlb_format), teamMapping[team], start.year))
+        data = json.load(response)
+        curEvents = data["schedule_team_sponsors"]["schedule_team_complete"]["queryResults"]["row"]
+        
+        for ev in curEvents:
+            s = datetime.datetime.strptime(ev["game_time_et"], "%Y-%m-%dT%H:%M:%S") + datetime.timedelta(hours=4)
+            e = s + datetime.timedelta(hours = 3)
+            teamStr = ev["team_brief"]
+            opponentStr = ev["opponent_brief"]
+            atHome = ev["home_away_sw"] == 'H'
+            if (atHome):
+                title = teamStr + " vs " + opponentStr
+            else:
+                title = teamStr + " @ " + opponentStr
+            gameStarted = ev["game_status_ind"] in ['F', 'I']
+            if gameStarted:
+                description = teamStr + ": " + ev['team_score'] + "<br>" + opponentStr + ": " + ev["opponent_score"]
+            else:
+                description = buildTeamDesc(teamStr, ev["team_wl"], ev["probable"], ev["probable_wl"], ev["probable_era"]) + "<br>"
+                description += buildTeamDesc(opponentStr, ev["opponent_wl"], ev["opp_probable"], ev["opp_probable_wl"], ev["opp_probable_era"])
+            events.append({'title': title, 'start': s.strftime(output_format), 'end': e.strftime(output_format), "description": description})
+    except Exception, ex:
         return JsonResponse(events, safe=False)
 
     return JsonResponse(events, safe=False)

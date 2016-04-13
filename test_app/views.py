7c66f0036fc1561f38e09eac45c475d417a9e2a2
Clayton Ritcher <claytonritcher@gmail.com>
2016-04-12 23:14:11 -0400

    Added a more descriptive description.

--- test_app/views.py
+++ test_app/views.py
@@ -6,6 +6,7 @@ import urllib2
 
 
 query = "https://api.seatgeek.com/2/events?datetime_utc.gte=%s&datetime_utc.lte=%s&taxonomies.name=concert&per_page=100&sort=score.desc"
+descStr = "Venue: <a href='https://www.google.com/maps/@%d,%d' target='_blank'>%s</a><br>%d tickets left on <a href='%s' target='_blank'>SeatGeek</a>"
 
 output_format = '%Y-%m-%dT%H:%MZ'
 input_format = '%Y-%m-%d'
@@ -30,19 +31,24 @@ def getEvents(request):
         data = json.load(response)
         if "events" in data:
             for ev in data["events"]:
-                if "date_tbd" in ev and ev["date_tbd"]:
-                    # no announced start/end time
-                    continue
-                if 'score' in ev and ev['score'] < .6:
-                    # low rated concert
-                    continue
-                title = ev["short_title"]
-                s = datetime.datetime.strptime(
-                    ev["datetime_utc"], "%Y-%m-%dT%H:%M:%S")
-                e = s + datetime.timedelta(hours=3)
-                description = ev["title"]
-                events.append({'title': title, 'start': s.strftime(
-                    output_format), 'end': e.strftime(output_format), "description": description})
+                try:
+                    if "date_tbd" in ev and ev["date_tbd"]:
+                        # no announced start/end time
+                        continue
+                    if 'score' in ev and ev['score'] < .6:
+                        # low rated concert
+                        continue
+                    title = ev["short_title"]
+                    s = datetime.datetime.strptime(
+                        ev["datetime_utc"], "%Y-%m-%dT%H:%M:%S")
+                    e = s + datetime.timedelta(hours=3)
+                    ven = ev['venue']
+                    #"Venue: <a href='https://www.google.com/maps/@%d,%d' target='_blank'>%s</a><br>%d tickets left on <a href='%s' target='_blank'>SeatGeek</a>"
+                    description = descStr % (ven['location']['lat'], ven['location']['lon'], ven['name'], ev['stats']['listing_count'], ev['url'])
+                    events.append({'title': title, 'start': s.strftime(
+                        output_format), 'end': e.strftime(output_format), "description": description})
+                except KeyError:
+                    pass
 
     except Exception, ex:
         print ex

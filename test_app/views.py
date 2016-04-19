0df47d82d931e7fa5723d1d612292c4156996de8
mikejeffers <mike.s.jeffers@gmail.com>
2016-04-19 16:36:08 -0400

    added fields for radius and popularity

--- test_app/views.py
+++ test_app/views.py
@@ -5,7 +5,7 @@ import datetime
 import urllib2
 
 
-query = "https://api.seatgeek.com/2/events?datetime_utc.gte=%s&datetime_utc.lte=%s&taxonomies.name=concert&per_page=100&sort=score.desc"
+query = "https://api.seatgeek.com/2/events?datetime_utc.gte=%s&datetime_utc.lte=%s&taxonomies.name=concert&per_page=100&range=%smi&sort=score.desc"
 descStr = "Venue: <a href='https://www.google.com/maps/@%f,%f,15z' target='_blank'>%s</a><br>%d tickets left on <a href='%s' target='_blank'>SeatGeek</a>"
 
 output_format = '%Y-%m-%dT%H:%MZ'
@@ -15,16 +15,23 @@ input_format = '%Y-%m-%d'
 @csrf_exempt
 def getEvents(request):
     loc = "15217"
+    minPopularity = 0.6
+    radius = 30
     try:
         tmp = json.loads(request.GET['settings'])
         loc = tmp["Zip Code"]["value"]
+        minPopularity = tmp["Popularity Threshold"]["value"]
+        radius = tmp["Range"]["value"]
+        radius = round(abs(radius))
+        if radius<1:
+            radius = 1
     except Exception, e:
         pass
     events = []
     try:
         start = request.GET['start']
         end = request.GET['end']
-        q = query % (start, end)
+        q = query % (start, end, str(radius))
         if loc is not "":
             q += "&postal_code=%s" % (loc)
         response = urllib2.urlopen(q)
@@ -35,7 +42,7 @@ def getEvents(request):
                     if "date_tbd" in ev and ev["date_tbd"]:
                         # no announced start/end time
                         continue
-                    if 'score' in ev and ev['score'] < .6:
+                    if 'score' in ev and ev['score'] < minPopularity:
                         # low rated concert
                         continue
                     title = ev["short_title"]
@@ -43,7 +50,8 @@ def getEvents(request):
                         ev["datetime_utc"], "%Y-%m-%dT%H:%M:%S")
                     e = s + datetime.timedelta(hours=3)
                     ven = ev['venue']
-                    description = descStr % (ven['location']['lat'], ven['location']['lon'], ven['name'], ev['stats']['listing_count'], ev['url'])
+                    description = descStr % (ven['location']['lat'], ven['location']['lon'], ven[
+                                             'name'], ev['stats']['listing_count'], ev['url'])
                     events.append({'title': title, 'start': s.strftime(
                         output_format), 'end': e.strftime(output_format), "description": description})
                 except KeyError:
@@ -74,5 +82,9 @@ def formHandling(request):
     else:
         return JsonResponse({"fields": [
             {"type": "number", "name": "Zip Code",
-                "required": True, "default": 15217}
+                "required": True, "default": 15217},
+            {"type": "choice", "name": "Popularity Threshold", 
+            "required": True, "choices": ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9"], 
+            "default": "2"},
+            {"type": "number", "name": "Range", "required": True, "default": 30} 
         ]})
